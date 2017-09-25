cb584100e9c989ca3c27bf730f95aa85e415aeb1
Cristian Rojas Acuña <cristian@debian>
2017-09-25 15:22:43 -0300

    Added component LastNewsItem
    -Added component LastNewsItem
    -Added component LastNewsCarousell
    -Modified Home for import Component LastNewsCarousel and children
    -
    	new file:   src/components/LastNewsCarousel/index.jsx
    	modified:   src/components/LastNewsItem/index.jsx
    	modified:   src/pages/Home/index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,48 +1,70 @@
 //Dependecies
-import React from 'react'
+import React, { Component } from 'react'
+import firebase from 'firebase'
 
 //Components
 import BigCarousel from '../../components/BigCarousel'
 import Event from '../../components/Event'
-import LastNewsItem from '../../components/LastNewsItem'
+import LastNewsCarousel from '../../components/LastNewsCarousel'
 
 //assets
 import styles from './home.css'
 
+class Home extends Component {
+  	constructor(props){
+    	super(props)
 
-const Home = () => {
-  return (
-    <div>
-      <BigCarousel />
-      <div className="container">
-        <section className={`${styles.sectionContent} ${"container"}`}>
-          <h5 className={styles.sectionHeading}>Últimas Noticias</h5>
-          <LastNewsItem />
-        </section>
-      </div>
+    	this.state = {
+     		 news:[]
+   	}
+  	}
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3">
-            <section className={styles.sectionContent}>
-              <h5 className={styles.sectionHeading}>Redes Sociales</h5>
-            </section>
-          </div>
-          <div className={"col-md-6"}>
-            <section className={styles.sectionContent}>
-              <h5 className={styles.sectionHeading}>Otro Contenido</h5>
-            </section>
-          </div>
-          <div className="col-md-3">
-            <section className={styles.sectionContent}>
-              <h5 className={styles.sectionHeading}>Próximos Eventos</h5>
-              <Event />
-            </section>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  componentWillMount() {
+    const newsRef = firebase.database().ref().child('news')
+    
+    newsRef.on('child_added', snapshot => {
+        this.setState({
+            news : this.state.news.concat(
+                snapshot.val()
+            )
+        	})
+   	})
+	  }
+	
+	render() {
+		return (
+			<div>
+			  <BigCarousel />
+			  <div className="container">
+				 <section className={`${styles.sectionContent} ${"container"}`}>
+					<h5 className={styles.sectionHeading}>Últimas Noticias</h5>
+					<LastNewsCarousel />
+				 </section>
+			  </div>
+	  
+			  <div className="container">
+				 <div className="row">
+					<div className="col-md-3">
+					  <section className={styles.sectionContent}>
+						 <h5 className={styles.sectionHeading}>Redes Sociales</h5>
+					  </section>
+					</div>
+					<div className={"col-md-6"}>
+					  <section className={styles.sectionContent}>
+						 <h5 className={styles.sectionHeading}>Otro Contenido</h5>
+					  </section>
+					</div>
+					<div className="col-md-3">
+					  <section className={styles.sectionContent}>
+						 <h5 className={styles.sectionHeading}>Próximos Eventos</h5>
+						 <Event />
+					  </section>
+					</div>
+				 </div>
+			  </div>
+			</div>
+		)
+	}	 
 }
 
 export default Home
