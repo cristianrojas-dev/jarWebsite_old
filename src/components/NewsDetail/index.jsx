06d0ccd795c89913837880fcafc0d4d2a17bf92f
Cristian Rojas Acuña <cristian@debian>
2017-09-25 17:51:57 -0300

    Add Component NewsDetail
    	new file:   src/components/NewsDetail/index.jsx

---
+++ src/components/NewsDetail/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class NewsDetail extends Component {
+    constructor(props){
+        super(props)
+        
+    }  
+    
+    render(){
+        return(
+            <div className="container">
+
+            </div>
+        )
+    }    
+}
+
+export default NewsDetail
